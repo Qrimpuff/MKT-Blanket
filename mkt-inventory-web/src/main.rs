07fbcb8e566dceb27d4a2ddf93aaeeb1284bda50
Qrimpuff <qrimpuff@gmail.com>
2021-10-21 22:34:27 -0400

    simple dark theme

--- mkt-inventory-web/src/main.rs
+++ mkt-inventory-web/src/main.rs
@@ -126,7 +126,7 @@ impl Component for App {
         };
         html! {
             <section class="section pt-4">
-                <nav class="navbar is-fixed-top is-warning" role="navigation" aria-label="main navigation">
+                <nav class="navbar is-fixed-top mkt-navbar" role="navigation" aria-label="main navigation">
                     <div class="navbar-brand">
                         <a class="navbar-item" href="">
                             <h1 class="title is-4">{ "MKT Inventory" }</h1>
